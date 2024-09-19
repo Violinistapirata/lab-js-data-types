7159ced80fc4442e4dcf92913e188b84e766d93a
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-09-20 00:15:44 +0200

    fix first iteration

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
-const s1 = "Fred ";
-const s2 = "fed ";
-const s3 = "Ted ";
-const s4 = "bread ";
-const s5 = "and ";
+const s1 = "Fred";
+const s2 = "fed";
+const s3 = "Ted";
+const s4 = "bread";
+const s5 = "and";
 
 // Concatenate the string variables into one new string
-const tongueTwister = s1+s2+s3+s4+s5+s3+s2+s1+s4
+const tongueTwister = s1+" "+s2+" "+s3+" "+s4+" "+s5+" "+s3+" "+s2+" "+s1+" "+s4+"."
 
 // Print out the concatenated string
 console.log(tongueTwister);
